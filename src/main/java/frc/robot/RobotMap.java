c809347346b75e6d55c119dc14c73134f3ba12a1
Alan Smith <alan.smith@nutanix.com>
2019-01-16 08:42:12 -0500

    Preparation for easy prototyping

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+public final class RobotMap {
+
+    // All CANs for Robot
+    // Example: public static final int CAN_CLIMBER = 13;
+
+    // All PWMs for Robot
+    // Example: public static final int PWM_DRIVETRAIN_FRONT_LEFT = 2;
+    public static final int PWM_TEST_ATTACHMENT = 3;
+
+    // Digital I/O
+    // Example: public static final int IO_UPPER_LIMIT_SW = 6;
+
+    // Motor Speeds
+    // Example: public static final double SPEED_DEFAULT_LIFT = 0.5;
+    public static final double SPEED_DEFAULT_TEST = 0.5;
+
+    // Joystick
+    // Example: public static final int JOYSTICK_DRIVE_LEFT = 0;
+    public static final int JOYSTICK_MAIN = 0;
+
+    // Joystick Buttons
+    // Example: public static final int BUTTON_LIFT_UP = 6;
+
+}
