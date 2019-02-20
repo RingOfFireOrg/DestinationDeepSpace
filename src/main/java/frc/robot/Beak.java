bcee8fc54048a5d345cecff65a3ff5cad495b3b4
Rugmini Remitha <rugminiremitha@gmail.com>
2019-02-19 20:36:03 -0500

    Fixed to work with beak

--- src/main/java/frc/robot/Beak.java
+++ src/main/java/frc/robot/Beak.java
@@ -2,14 +2,19 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Beak {
     private boolean isOpen;
-    private Servo beakActuator = new Servo(RobotMap.BEAK_ACTUATOR_CHANNEL);
+    private Servo beakActuator; //= new Servo(RobotMap.BEAK_ACTUATOR_CHANNEL);
 
     private static Beak beak;
 
     protected Beak() {
+        beakActuator = new Servo(RobotMap.BEAK_ACTUATOR_CHANNEL);
+        // AlanS - this needs setBounds or no good....
+        beakActuator.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
+        isOpen = false;
     }
 
     public static Beak getInstance() {
@@ -20,14 +25,12 @@ public class Beak {
     }
 
     void open() {
-        beakActuator.set(1);
-        DriverStation.reportError("Beak Open", false);
+        beakActuator.setSpeed(1.0);
         isOpen = true;
     }
 
     void close(){
-        beakActuator.set(0);
-        DriverStation.reportError("Beak Close", false);
+        beakActuator.setSpeed(-1.0);
         isOpen = false;
     }
 
