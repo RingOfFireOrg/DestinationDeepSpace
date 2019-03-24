a82fc9be679a4af154729bca39397a87cb9f92ff
Connor Mitchell <connorbrannonmitchell@gmail.com>
2019-03-24 17:32:44 -0400

    stuff connor added sunday afternoon in awards ceremony - uncalled so shouldn't mess anything up

--- src/main/java/frc/robot/SwerveAutomation.java
+++ src/main/java/frc/robot/SwerveAutomation.java
@@ -4,16 +4,22 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class SwerveAutomation {
 
-    private static SwerveAutomation swerveAutomation;
-    
-    SwerveAutomation (AHRS ahrs, SwerveDrive swerveDrive) {
+	private static SwerveAutomation swerveAutomation;
 
-    }
+	SwerveAutomation(AHRS ahrs, SwerveDrive swerveDrive) {
 
-    public static SwerveAutomation getInstance(AHRS ahrs, SwerveDrive swerveDrive) {
+	}
+
+	public static SwerveAutomation getInstance(AHRS ahrs, SwerveDrive swerveDrive) {
 		if (swerveAutomation == null) {
 			swerveAutomation = new SwerveAutomation(ahrs, swerveDrive);
 		}
 		return swerveAutomation;
+	}
+
+	public static Point translationVector() {
+		if (swerveDrive.frontRightCMPerSecond() != null && swerveDrive.backLeftCMPerSecond != null) {
+			
+		}
 	}
 }
