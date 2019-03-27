03bc0cce69fb28473e8f2f32c41827947d3c7aed
ConnorMitchell314 <connorbrannonmitchell@gmail.com>
2019-03-27 18:14:07 -0400

    finished the cm per second translation vector

--- src/main/java/frc/robot/SwerveAutomation.java
+++ src/main/java/frc/robot/SwerveAutomation.java
@@ -22,30 +22,46 @@ public class SwerveAutomation {
 		return swerveAutomation;
 	}
 
-	public Point translationVector() {
-		//returns a point of the translation vector in a robot relative
-		if (swerveDrive.frontRightCMPerSecond() != 0 && swerveDrive.frontLeftCMPerSecond() != 0 && swerveDrive.backLeftCMPerSecond() != 0 && swerveDrive.backRightCMPerSecond() != 0) { //the != should be to what the encoders return if they are not working
-			Point frWheelOriginalVector = new Point(RobotMap.ROBOT_STANCE_X_IN_CM / 2, RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
-			Point flWheelOriginalVector = new Point(-RobotMap.ROBOT_STANCE_X_IN_CM / 2, RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
-			Point blWheelOriginalVector = new Point(-RobotMap.ROBOT_STANCE_X_IN_CM / 2, -RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
-			Point brWheelOriginalVector = new Point(RobotMap.ROBOT_STANCE_X_IN_CM / 2, -RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
-			
-			Point frWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.frontRightCMPerSecond()), swerveDrive.frontRightAngle());
-			Point flWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.frontLeftCMPerSecond()), swerveDrive.frontLeftAngle());
-			Point blWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.backLeftCMPerSecond()), swerveDrive.backLeftAngle());
-			Point brWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.backRightCMPerSecond()), swerveDrive.backRightAngle());
+	public Point translationVectorInCM() {
+		// returns a point of the translation vector in a robot relative
+		Point frWheelOriginalVector = new Point(RobotMap.ROBOT_STANCE_X_IN_CM / 2, RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
+		Point flWheelOriginalVector = new Point(-RobotMap.ROBOT_STANCE_X_IN_CM / 2, RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
+		Point blWheelOriginalVector = new Point(-RobotMap.ROBOT_STANCE_X_IN_CM / 2, -RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
+		Point brWheelOriginalVector = new Point(RobotMap.ROBOT_STANCE_X_IN_CM / 2, -RobotMap.ROBOT_STANCE_Y_IN_CM / 2);
 
-			Point frWheelTransformedVector = GeometricMath.vectorAddition(frWheelOriginalVector, frWheelOriginalVector);
-			Point flWheelTransformedVector = GeometricMath.vectorAddition(flWheelOriginalVector, flWheelOriginalVector);
-			Point blWheelTransformedVector = GeometricMath.vectorAddition(blWheelOriginalVector, blWheelOriginalVector);
-			Point brWheelTransformedVector = GeometricMath.vectorAddition(brWheelOriginalVector, brWheelOriginalVector);
+		Point frWheelTransformationVector = GeometricMath
+				.rotateVector(new Point(0, swerveDrive.frontRightCMPerSecond()), swerveDrive.frontRightAngle());
+		Point flWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.frontLeftCMPerSecond()),
+				swerveDrive.frontLeftAngle());
+		Point blWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.backLeftCMPerSecond()),
+				swerveDrive.backLeftAngle());
+		Point brWheelTransformationVector = GeometricMath.rotateVector(new Point(0, swerveDrive.backRightCMPerSecond()),
+				swerveDrive.backRightAngle());
 
-			Point originalCenter = GeometricMath.midPoint(GeometricMath.midPoint(frWheelOriginalVector, blWheelOriginalVector), GeometricMath.midPoint(flWheelOriginalVector, brWheelOriginalVector));
-			Point translatedCenter = GeometricMath.midPoint(GeometricMath.midPoint(frWheelTransformedVector, blWheelTransformedVector), GeometricMath.midPoint(flWheelTransformedVector, brWheelTransformedVector));
+		Point frWheelTransformedVector = GeometricMath.vectorAddition(frWheelOriginalVector, frWheelOriginalVector);
+		Point flWheelTransformedVector = GeometricMath.vectorAddition(flWheelOriginalVector, flWheelOriginalVector);
+		Point blWheelTransformedVector = GeometricMath.vectorAddition(blWheelOriginalVector, blWheelOriginalVector);
+		Point brWheelTransformedVector = GeometricMath.vectorAddition(brWheelOriginalVector, brWheelOriginalVector);
 
+		if (swerveDrive.frSpeedEncoderIsWorking && swerveDrive.flSpeedEncoderIsWorking
+				&& swerveDrive.blSpeedEncoderIsWorking && swerveDrive.brSpeedEndoderIsWorking) {
+			Point originalCenter = GeometricMath.midPoint(
+					GeometricMath.midPoint(frWheelOriginalVector, blWheelOriginalVector),
+					GeometricMath.midPoint(flWheelOriginalVector, brWheelOriginalVector));
+			Point translatedCenter = GeometricMath.midPoint(
+					GeometricMath.midPoint(frWheelTransformedVector, blWheelTransformedVector),
+					GeometricMath.midPoint(flWheelTransformedVector, brWheelTransformedVector));
+			return GeometricMath.vectorSubtraction(translatedCenter, originalCenter);
+		} else if (swerveDrive.frSpeedEncoderIsWorking && swerveDrive.blSpeedEncoderIsWorking) {
+			Point originalCenter = GeometricMath.midPoint(frWheelOriginalVector, blWheelOriginalVector);
+			Point translatedCenter = GeometricMath.midPoint(frWheelTransformedVector, blWheelTransformedVector);
+			return GeometricMath.vectorSubtraction(translatedCenter, originalCenter);
+		} else if (swerveDrive.flSpeedEncoderIsWorking && swerveDrive.brSpeedEndoderIsWorking) {
+			Point originalCenter = GeometricMath.midPoint(flWheelOriginalVector, brWheelOriginalVector);
+			Point translatedCenter = GeometricMath.midPoint(flWheelTransformedVector, brWheelTransformedVector);
 			return GeometricMath.vectorSubtraction(translatedCenter, originalCenter);
 		} else {
-			
+			return new Point(0, 0);
 		}
 
 	}
